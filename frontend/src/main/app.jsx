0792856398cb3868c37b0fb1252247c0e6d18bf4
Renato Martins <renatto.martins@gmail.com>
2019-02-12 07:57:10 -0200

    Includes react componentes to page template

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,7 +1,14 @@
+import '../common/template/dependencies'
 import React from 'react'
+
+import Header from '../common/template/header'
+import SideBar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
 
 export default props => (
     <div className='wrapper'>
-        <h1>App</h1>
+        <Header />
+        <SideBar />
+        <Footer />
     </div>
 )
