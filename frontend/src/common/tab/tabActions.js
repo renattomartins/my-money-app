f537c3b04e5aa21fb177fa4b15656b36188d6073
Renato Martins <renatto.martins@gmail.com>
2019-08-06 20:25:55 -0300

    Creates action to show visible tabs

--- frontend/src/common/tab/tabActions.js
+++ frontend/src/common/tab/tabActions.js
@@ -1,6 +1,17 @@
+import tabs from "./tabs";
+
 export function selectTab(tabId) {
     return {
         type: 'TAB_SELECTED',
         payload: tabId
+    }
+}
+
+export function showTabs(...tabIds) {
+    const tabsToShow = {}
+    tabIds.forEach(e => tabsToShow[e] = true)
+    return {
+        type: 'TAB_SHOWED',
+        payload: tabsToShow
     }
 }
