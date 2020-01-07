4d6b051464c8d1edbcb42f227bcade3a8686a01c
Renato Martins <renatto.martins@gmail.com>
2020-01-07 19:31:39 -0300

    Includes add and clone buttons to credit list component

--- frontend/src/billingCycle/creditList.jsx
+++ frontend/src/billingCycle/creditList.jsx
@@ -1,21 +1,37 @@
 import React, { Component } from 'react'
-import { Field } from 'redux-form'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { Field, arrayInsert } from 'redux-form'
 import Grid from '../common/layout/grid'
 import Input from '../common/form/input'
 
 class CreditList extends Component {
+
+    add(index, item = {}) {
+        if(!this.props.readOnly) {
+            this.props.arrayInsert('billingCycleForm', 'credits', index, item)
+        }
+    }
 
     renderRows() {
         const list = this.props.list || []
         return list.map((item, index) => (
             <tr key={index}>
                 <td><Field name={`credits[${index}].name`} component={Input}
-                    placeHolder='Informe o nome' readOnly={this.props.readOnly} /></td>
+                    placeholder='Informe o nome' readOnly={this.props.readOnly} /></td>
                 <td><Field name={`credits[${index}].value`} component={Input}
-                    placeHolder='Informe o valor' readOnly={this.props.readOnly} /></td>
-                <td></td>
+                    placeholder='Informe o valor' readOnly={this.props.readOnly} /></td>
+                <td>
+                    <button type='button' className='btn btn-success'
+                        onClick={() => this.add(index +1)}>
+                        <i className="fa fa-plus"></i>
+                    </button>
+                    <button type='button' className='btn btn-warning'
+                        onClick={() => this.add(index +1, item)}>
+                        <i className="fa fa-clone"></i>
+                    </button>
+                </td>
             </tr>
-
         ))
     }
 
@@ -24,22 +40,23 @@ class CreditList extends Component {
             <Grid cols={this.props.cols}>
                 <fieldset>
                     <legend>Créditos</legend>
+                    <table className='table'>
+                        <thead>
+                            <tr>
+                                <th>Nome</th>
+                                <th>Valor</th>
+                                <th className='table-actions'>Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.renderRows()}
+                        </tbody>
+                    </table>
                 </fieldset>
-                <table className='table'>
-                    <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>Valor</th>
-                            <th>Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.renderRows()}
-                    </tbody>
-                </table>
             </Grid>
         )
     }
 }
 
-export default CreditList
+const mapDispatchToProps = dispatch => bindActionCreators({ arrayInsert }, dispatch)
+export default connect(null, mapDispatchToProps)(CreditList)
