e16f711387cd4ed420ee97615ec190bae6313e58
Renato Martins <renatto.martins@gmail.com>
2020-01-07 20:38:06 -0300

    Adds the summary component inside billingCycle form

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -6,6 +6,7 @@ import { reduxForm, Field, formValueSelector } from 'redux-form'
 import { init } from './billingCycleActions'
 import LabelAndInput from '../common/form/labelAndInput'
 import ItemList from './itemList'
+import Summary from './summary'
 
 class BillingCycleForm extends Component {
 
@@ -17,12 +18,15 @@ class BillingCycleForm extends Component {
                     <Field name='name' component={LabelAndInput} readOnly={readOnly}
                         label='Nome' cols='12 4' placeholder='Informe o nome' />
                     <Field name='month' component={LabelAndInput} type='number' readOnly={readOnly}
-                        label='Mês' cols='12 4' placeholder='Informe o mês'  />
+                        label='Mês' cols='12 4' placeholder='Informe o mês' />
                     <Field name='year' component={LabelAndInput} type='number' readOnly={readOnly}
-                        label='Ano' cols='12 4' placeholder='Informe o ano'  />
+                        label='Ano' cols='12 4' placeholder='Informe o ano' />
+                    <div className="clearfix">
+                        <Summary credit={1000} debt={100} />
+                    </div>
                     <ItemList cols='12 6' list={credits} readOnly={readOnly}
                         field='credits' legend='Créditos' />
-                        <ItemList cols='12 6' list={debts} readOnly={readOnly}
+                    <ItemList cols='12 6' list={debts} readOnly={readOnly}
                         field='debts' legend='Débitos' showStatus={true} />
                 </div>
                 <div className='box-footer'>
--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Router, Route, Redirect, hashHistory } from 'react-router'
 
-import Dashboard from '../reactOnlyDashboard/dashboard'
+import Dashboard from '../reduxDashboard/dashboard'
 import BillingCycle from '../billingCycle/billingCycle'
 
 export default props => (
